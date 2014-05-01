c9dbc765cb1490b5e6349dc4755d1d099b189eb5
Marc Hibbins <marchibbins@gmail.com>
2014-05-01 12:02:39 +0100

    Clean up view

--- index/views.py
+++ index/views.py
@@ -7,7 +7,7 @@ def list(request):
 
     sort = request.GET.get('sort', 'last_name')
     if sort.split('-').pop() not in ['first_name', 'last_name', 'email']:
-         sort = 'last_name'
+        sort = 'last_name'
 
     users = User.objects.order_by(sort)
     return render(request, 'index.html', {'users': users})
