6f608812adb73c134f9d05894b187cf48d9d7d8f
garethfoote <gareth.foote@gmail.com>
2014-05-19 14:06:35 +0100

    Sorting based on foreign key fields

--- index/views.py
+++ index/views.py
@@ -1,13 +1,20 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render
 
+from index.models import Tenant
 
 def list(request):
     """ Renders sortable list of Users. """
 
+    user_fields = ['first_name', 'last_name', 'email']
     sort = request.GET.get('sort', 'last_name')
-    if sort.split('-').pop() not in ['first_name', 'last_name', 'email']:
-        sort = 'last_name'
+    reverse = (sort[0:1] == '-')
 
-    users = User.objects.order_by(sort)
-    return render(request, 'index.html', {'users': users})
+    if sort.split('-').pop() in user_fields:
+        sort = 'user__'+sort.split('-').pop()
+        if reverse == True:
+            sort = '-'+sort
+
+    # import pdb; pdb.set_trace()
+    tenants = Tenant.objects.order_by(sort)
+    return render(request, 'index.html', {'tenants': tenants})
