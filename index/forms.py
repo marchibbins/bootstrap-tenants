aacf50ef5c09dce29dbffc36dc182fcbf8a606bf
Marc Hibbins <marchibbins@gmail.com>
2014-07-03 15:45:35 +0100

    Randomly generate and popular User password on admin view

--- index/forms.py
+++ index/forms.py
@@ -1,5 +1,5 @@
+from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from django.forms import ModelForm
 from index.models import CustomUser
 
 
@@ -10,9 +10,19 @@ class CustomUserCreationForm(UserCreationForm):
     and adding email, first name and last name.
     """
 
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput(render_value=True),
+        help_text='This password has been randomly generated, only edit this if you want to manually choose a password.')
+    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput(render_value=True),
+        help_text='Enter the same password as above (if changed), for verification.')
+
     def __init__(self, *args, **kargs):
         super(CustomUserCreationForm, self).__init__(*args, **kargs)
         del self.fields['username']
+
+        # Generate random password, user will be notified with reset email.
+        password = CustomUser.objects.make_random_password()
+        self.fields['password1'].initial = password
+        self.fields['password2'].initial = password
 
     class Meta:
         model = CustomUser
@@ -32,7 +42,7 @@ class CustomUserChangeForm(UserChangeForm):
         model = CustomUser
 
 
-class CustomUserUpdateForm(ModelForm):
+class CustomUserUpdateForm(forms.ModelForm):
 
     """
     Simple front-facing CustomUser form for non-staff use.
