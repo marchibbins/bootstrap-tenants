1e6598deabf9c4fb1365f3e38daa8db4af3558df
Marc Hibbins <marchibbins@gmail.com>
2014-07-04 11:04:55 +0100

    Update language code

--- bootstraptenants/settings.py
+++ bootstraptenants/settings.py
@@ -96,7 +96,7 @@ CSRF_FAILURE_VIEW = 'index.views.error403'
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-gb'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_L10N = True
