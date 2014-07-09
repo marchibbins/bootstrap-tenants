70c7f6f46433be27b108875c9136dd602ec65815
garethfoote <gareth.foote@gmail.com>
2014-07-09 11:42:36 +0100

    Extending settings

--- bootstraptenants/develop_gf_settings.py
+++ bootstraptenants/develop_gf_settings.py
@@ -1,25 +1,4 @@
-"""
-Django settings for bootstraptenants project.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.6/ref/settings/
-"""
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
-SITE_ID = 1
-SITE_SECURE = False
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=vzzml7lofbwe_ob-&gi1#ci$_vng+x(zo!fcifq5-0)bh8i5b'
+from settings import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -42,25 +21,6 @@ INSTALLED_APPS = (
     'inspector_panel',
 )
 
-if DEBUG:
-    CACHES = {
-        'default': {
-            'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
-        }
-    }
-
-MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-)
-
-ROOT_URLCONF = 'bootstraptenants.urls'
-WSGI_APPLICATION = 'bootstraptenants.wsgi.application'
-
 DEBUG_TOOLBAR_PANELS = (
     'debug_toolbar.panels.versions.VersionsPanel',
     'debug_toolbar.panels.timer.TimerPanel',
@@ -77,7 +37,6 @@ DEBUG_TOOLBAR_PANELS = (
     'inspector_panel.panels.inspector.InspectorPanel',
 )
 
-
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 # Parse database configuration from $DATABASE_URL
@@ -92,36 +51,6 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
-# Auth
-AUTH_USER_MODEL = 'index.CustomUser'
-LOGIN_URL = '/login'
-LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/'
-
-CSRF_FAILURE_VIEW = 'index.views.error403'
-
-# Internationalization
-# https://docs.djangoproject.com/en/1.6/topics/i18n/
-LANGUAGE_CODE = 'en-gb'
-TIME_ZONE = 'UTC'
-USE_I18N = True
-USE_L10N = True
-USE_TZ = True
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
-STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
-
-MEDIA_ROOT = 'media'
-MEDIA_URL = '/media/'
-
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-# Allow all host headers
-ALLOWED_HOSTS = ['*']
 
 # Email
 if DEBUG:
