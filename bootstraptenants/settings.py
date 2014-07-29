d6eb5a350b1d9f733bb0fbb90a2d8db615987047
Marc Hibbins <marchibbins@gmail.com>
2014-07-29 15:44:35 +0100

    Use email TLS environ

--- bootstraptenants/settings.py
+++ bootstraptenants/settings.py
@@ -126,6 +126,7 @@ EMAIL_HOST = os.environ.get('EMAIL_HOST', '')
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
 EMAIL_PORT = os.environ.get('EMAIL_PORT', 25)
+EMAIL_USE_TLS = bool(os.environ.get('EMAIL_USE_TLS', False))
 
 DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
 SERVER_EMAIL = os.environ.get('SERVER_EMAIL', DEFAULT_FROM_EMAIL)
