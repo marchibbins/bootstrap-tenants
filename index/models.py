5361d1a1f51d647ca6a8481c18fdc230a35cc9b9
Marc Hibbins <marchibbins@gmail.com>
2014-08-06 12:31:56 +0100

    Rename staff label on custom User model

--- index/models.py
+++ index/models.py
@@ -22,7 +22,7 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     last_name = models.CharField('last name', max_length=30)
     date_joined = models.DateTimeField('date joined', default=timezone.now)
 
-    is_staff = models.BooleanField('staff status', default=False,
+    is_staff = models.BooleanField('admin status', default=False,
         help_text='Designates whether the user can log into this admin site.')
     is_active = models.BooleanField('active', default=True,
         help_text='Designates whether this user should be treated as active. Unselect this instead of deleting accounts.')
